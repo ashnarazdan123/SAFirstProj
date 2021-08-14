b1db5d07f5fce6050040eb557b32c7df28165dc3
20szha <szha@bu.edu>
2021-08-13 22:00:31 -0400

    New Changes

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Susan and Ashna's First Project!!!
+          Ashna's and Susan's First Project!!!
         </p>
-
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
